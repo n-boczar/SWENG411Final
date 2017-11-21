f61c19a2fb4b68d487dcd2309692f7b682442691
mtw5280 <33348829+mtw5280@users.noreply.github.com>
2017-11-21 15:55:38 -0500

    Added Table

--- TheBehrendCasino/src/roullette/RoulletteWheel.java
+++ TheBehrendCasino/src/roullette/RoulletteWheel.java
@@ -3,9 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package roullette;
 
+
+
+
+
+package roullette;
 import java.awt.Graphics;
+import java.awt.Color;
 
 /**
  *
@@ -31,35 +36,27 @@ public class RoulletteWheel extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton9 = new javax.swing.JButton();
         jButton10 = new javax.swing.JButton();
         jButton11 = new javax.swing.JButton();
         jButton12 = new javax.swing.JButton();
         jButton14 = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
+        jButton9 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jButton9.setFont(new java.awt.Font("Tahoma", 1, 21)); // NOI18N
-        jButton9.setIcon(new javax.swing.ImageIcon("C:\\Users\\mtw52\\Pictures\\RoullettePics\\LBchipSmall.png")); // NOI18N
-        jButton9.setText("5");
-        jButton9.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton9MouseClicked(evt);
-            }
-        });
-        jButton9.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton9ActionPerformed(evt);
-            }
-        });
 
         jButton10.setFont(new java.awt.Font("Tahoma", 1, 21)); // NOI18N
         jButton10.setIcon(new javax.swing.ImageIcon("C:\\Users\\mtw52\\Pictures\\RoullettePics\\FinalChipSmall2.png")); // NOI18N
         jButton10.setText("10");
+        jButton10.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButton10MouseReleased(evt);
+            }
+        });
         jButton10.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton10ActionPerformed(evt);
@@ -69,6 +66,11 @@ public class RoulletteWheel extends javax.swing.JFrame {
         jButton11.setFont(new java.awt.Font("Tahoma", 1, 21)); // NOI18N
         jButton11.setIcon(new javax.swing.ImageIcon("C:\\Users\\mtw52\\Pictures\\RoullettePics\\OrangeChipSmall2.png")); // NOI18N
         jButton11.setText("50");
+        jButton11.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButton11MouseReleased(evt);
+            }
+        });
         jButton11.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton11ActionPerformed(evt);
@@ -78,6 +80,11 @@ public class RoulletteWheel extends javax.swing.JFrame {
         jButton12.setFont(new java.awt.Font("Tahoma", 1, 21)); // NOI18N
         jButton12.setIcon(new javax.swing.ImageIcon("C:\\Users\\mtw52\\Pictures\\RoullettePics\\RedChipSmall2.png")); // NOI18N
         jButton12.setText("20");
+        jButton12.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButton12MouseReleased(evt);
+            }
+        });
         jButton12.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton12ActionPerformed(evt);
@@ -87,24 +94,35 @@ public class RoulletteWheel extends javax.swing.JFrame {
         jButton14.setFont(new java.awt.Font("Tahoma", 1, 21)); // NOI18N
         jButton14.setIcon(new javax.swing.ImageIcon("C:\\Users\\mtw52\\Pictures\\RoullettePics\\BlackChipSmall.png")); // NOI18N
         jButton14.setText("100");
+        jButton14.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButton14MouseReleased(evt);
+            }
+        });
         jButton14.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton14ActionPerformed(evt);
             }
         });
 
-        jPanel1.setBackground(new java.awt.Color(51, 51, 255));
+        jPanel1.setBackground(new java.awt.Color(15, 89, 0));
         jPanel1.setForeground(new java.awt.Color(51, 255, 0));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\mtw52\\Pictures\\RoullettePics\\roulette-table3.png")); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1166, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel2)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 724, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel2))
         );
 
         jTextField1.setText("jTextField1");
@@ -122,17 +140,42 @@ public class RoulletteWheel extends javax.swing.JFrame {
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGap(0, 754, Short.MAX_VALUE)
         );
+
+        jButton9.setFont(new java.awt.Font("Tahoma", 1, 21)); // NOI18N
+        jButton9.setIcon(new javax.swing.ImageIcon("C:\\Users\\mtw52\\Pictures\\RoullettePics\\LBchipSmall.png")); // NOI18N
+        jButton9.setText("5");
+        jButton9.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                jButton9MouseDragged(evt);
+            }
+        });
+        jButton9.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton9MouseClicked(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jButton9MousePressed(evt);
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jButton9MouseReleased(evt);
+            }
+        });
+        jButton9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton9ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(29, 29, 29)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton9, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -149,16 +192,15 @@ public class RoulletteWheel extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jTextField1))
                     .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(40, 40, 40))
+                .addGap(10, 10, 10))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                         .addGap(16, 16, 16)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -168,8 +210,9 @@ public class RoulletteWheel extends javax.swing.JFrame {
                                 .addComponent(jButton11, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addComponent(jButton12, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addComponent(jButton10, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(jButton9, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(jButton14, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(jButton14, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jButton9, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(91, 91, 91))
         );
 
@@ -201,6 +244,91 @@ public class RoulletteWheel extends javax.swing.JFrame {
         // TODO add your handling code here:
      
     }//GEN-LAST:event_jButton9MouseClicked
+
+    
+    private void jButton9MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton9MousePressed
+        // TODO add your handling code here:
+        
+        
+    }//GEN-LAST:event_jButton9MousePressed
+
+    private void jButton9MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton9MouseReleased
+        Graphics g = this.getGraphics();
+        int x = evt.getXOnScreen(); //Chip X Coordinate
+        int y = evt.getYOnScreen(); //Chip Y Coordinate
+        int x2 = x + 5; //Inner Circle Coordinate
+        int y2 = y + 5; //Inner Circle Coordinate
+                
+        g.setColor(Color.CYAN);
+        g.fillOval(x,y,40,40);
+        g.setColor(Color.WHITE);
+        g.fillOval(x2,y2,30,30);
+    }//GEN-LAST:event_jButton9MouseReleased
+
+    private void jButton9MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton9MouseDragged
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jButton9MouseDragged
+
+    private void jButton10MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton10MouseReleased
+        // TODO add your handling code here:
+        Graphics g = this.getGraphics();
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        
+        g.setColor(Color.BLUE);
+        g.fillOval(x,y,40,40);
+        
+        int x2 = x + 5; //Inner Circle Coordinate
+        int y2 = y + 5; //Inner Circle Coordinate
+        g.setColor(Color.WHITE);
+        g.fillOval(x2,y2,30,30);
+    }//GEN-LAST:event_jButton10MouseReleased
+
+    private void jButton12MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton12MouseReleased
+        // TODO add your handling code here:
+        Graphics g = this.getGraphics();
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        
+        g.setColor(Color.RED);
+        g.fillOval(x,y,40,40);
+        
+        int x2 = x + 5; //Inner Circle Coordinate
+        int y2 = y + 5; //Inner Circle Coordinate
+        g.setColor(Color.WHITE);
+        g.fillOval(x2,y2,30,30);
+    }//GEN-LAST:event_jButton12MouseReleased
+
+    private void jButton11MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton11MouseReleased
+        // TODO add your handling code here:
+        Graphics g = this.getGraphics();
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        
+        g.setColor(Color.ORANGE);
+        g.fillOval(x,y,40,40);
+        
+        int x2 = x + 5; //Inner Circle Coordinate
+        int y2 = y + 5; //Inner Circle Coordinate
+        g.setColor(Color.WHITE);
+        g.fillOval(x2,y2,30,30);
+    }//GEN-LAST:event_jButton11MouseReleased
+
+    private void jButton14MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton14MouseReleased
+        // TODO add your handling code here:
+        Graphics g = this.getGraphics();
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        
+        g.setColor(Color.BLACK);
+        g.fillOval(x,y,40,40);
+        
+        int x2 = x + 5; //Inner Circle Coordinate
+        int y2 = y + 5; //Inner Circle Coordinate
+        g.setColor(Color.WHITE);
+        g.fillOval(x2,y2,30,30);
+    }//GEN-LAST:event_jButton14MouseReleased
 
     /**
      * @param args the command line arguments
@@ -245,6 +373,7 @@ public class RoulletteWheel extends javax.swing.JFrame {
     private javax.swing.JButton jButton14;
     private javax.swing.JButton jButton9;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JTextField jTextField1;
