d48523f642325d74743263a3393336cc5c8ccb6b
Perry Chambers <pkc5102@psu.edu>
2017-12-02 17:04:54 -0500

    Added basic help frame for all games.
    Button sends uses to web page

--- TheBehrendCasino/src/View/BlackJackFrame.java
+++ TheBehrendCasino/src/View/BlackJackFrame.java
@@ -84,6 +84,7 @@ public class BlackJackFrame extends javax.swing.JFrame {
         jMenu1 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
 
         jButton4.setText("Bet");
@@ -209,6 +210,14 @@ public class BlackJackFrame extends javax.swing.JFrame {
             }
         });
         jMenu1.add(jMenuItem1);
+
+        jMenuItem3.setText("Help");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem3);
 
         jMenuBar1.add(jMenu1);
 
@@ -422,7 +431,13 @@ public class BlackJackFrame extends javax.swing.JFrame {
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
          GameSelectionFrame.startIt(p);
+         this.dispose();
     }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+        new HelpScreen().setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     public int getDealerMove() throws IOException{
         int move;
@@ -526,6 +541,7 @@ public class BlackJackFrame extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel4;
