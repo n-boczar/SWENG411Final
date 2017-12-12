1c366af0f7ec9e06b97a9c7348bd476da1f59a8f
n-boczar <33349148+n-boczar@users.noreply.github.com>
2017-12-12 17:57:21 -0500

    dispose of game selection screen when a game starts

--- TheBehrendCasino/src/View/GameSelectionFrame.java
+++ TheBehrendCasino/src/View/GameSelectionFrame.java
@@ -187,15 +187,18 @@ public class GameSelectionFrame extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
        FCPokerFrame.startIt(player, true);
+       this.dispose();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         TexasHoldemFrame.startIt(player, true);
+        this.dispose();
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
         RoulletteWheel.startIt(player, true);
+        this.dispose();
     }//GEN-LAST:event_jButton6ActionPerformed
 
 //    public static void setCurrency(String s){
